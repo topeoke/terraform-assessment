1aeac5f5f7e8c41d451375482df4be3832db3437
= <=>
2021-09-20 21:17:17 +0100

    added vpc module to the main file

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider aws {
+  region = "eu-west-1"
+  shared_credentials_file = "/Users/temi/.aws/credentials"
+}
+
+
+
+}
+
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  name = "Publics Sapient Assessment VPC"
+  cidr = "172.16.0.0/16"
+  azs             = ["eu-west-1a", "eu-west-1b"]
+  private_subnets = ["172.16.0.0/24", "172.16.1.0/24"]
+  public_subnets  = ["172.16.2.0/24", "172.16.3.0/24"]
+
+
+  tags = {
+    Environment = "Production"
+    Client = "Publics Sapient"
+  }
+}
